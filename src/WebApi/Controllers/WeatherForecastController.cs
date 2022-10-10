439138553f9ff556ba0c6111b3a5d9567cbc9e17
Gordey Stakhovets <gordeystahovets@gmail.com>
2022-10-10 13:03:27 +0300

    feat(webapi): add initial controllers

---
+++ src/WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection.WeatherForecasts.Queries.GetWeatherForecasts;
+
+namespace WebApi.Controllers;
+
+public class WeatherForecastController : ApiControllerBase
+{
+    [HttpGet]
+    public async Task<IEnumerable<WeatherForecast>> Get() => 
+        await Mediator.Send(new GetWeatherForecastsQuery());
+}
