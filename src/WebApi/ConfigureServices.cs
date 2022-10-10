d2f4efd08994246ddd8f32c66edddd71baae9899
Gordey Stakhovets <gordeystahovets@gmail.com>
2022-10-10 13:03:04 +0300

    feat(packages): add and configure initial packages

---
+++ src/WebApi/ConfigureServices.cs
@@ -0,0 +1,14 @@
+﻿namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddWebApiServices(this IServiceCollection services)
+    {
+        services.AddHealthChecks();
+        services.AddControllers();
+        
+        services.AddSwaggerGen();
+
+        return services;
+    }
+}
--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,6 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationServices();
+builder.Services.AddWebApiServices();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHealthChecks("/health");
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
