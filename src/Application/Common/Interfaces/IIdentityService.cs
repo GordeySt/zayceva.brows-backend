97ef942da3ade96ea62ed19a2f6e875ea5f844d0
Gordey Stakhovets <gordeystahovets@gmail.com>
2022-10-12 00:47:40 +0300

    feat: add identity service and interfaces to communicate between infrastructure and application layers

---
+++ src/Application/Common/Interfaces/IIdentityService.cs
@@ -0,0 +1,10 @@
+﻿using Application.Common.Models;
+
+namespace Application.Common.Interfaces;
+
+public interface IIdentityService
+{
+    Task<(Result result, Guid userId)> CreateUserAsync(string email, string password, string phoneNumber);
+
+    Task AddUserToRole(Guid userId, string role);
+}
---
+++ src/Infrastructure/Identity/IdentityService.cs
@@ -0,0 +1,38 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Identity;
+
+public class IdentityService : IIdentityService
+{
+
+    private readonly UserManager<AppUser> _userManager;
+    
+    public IdentityService(UserManager<AppUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<(Result result, Guid userId)> CreateUserAsync(string email, string password, string phoneNumber)
+    {
+        var user = new AppUser
+        {
+            UserName = email,
+            Email = email,
+            PhoneNumber = phoneNumber
+        };
+
+        var result = await _userManager.CreateAsync(user, password);
+
+        return (result.ToApplicationResult(), user.Id);
+    }
+
+    public async Task AddUserToRole(Guid userId, string role)
+    {
+        var user = _userManager.Users.SingleOrDefault(u => u.Id == userId);
+
+        if (user is not null)
+            await _userManager.AddToRoleAsync(user, role);
+    }
+}
