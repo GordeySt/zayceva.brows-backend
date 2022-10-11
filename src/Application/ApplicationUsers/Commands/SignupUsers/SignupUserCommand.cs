3d280adf15e503b190b29c8514d2d0e6f5d425f6
Gordey Stakhovets <gordeystahovets@gmail.com>
2022-10-12 00:48:02 +0300

    feat: add initial logic for signing up user

---
+++ src/Application/ApplicationUsers/Commands/SignupUsers/SignupUserCommand.cs
@@ -0,0 +1,34 @@
+﻿using Application.Common.Interfaces;
+using Application.Constants;
+using MediatR;
+
+namespace Application.ApplicationUsers.Commands.SignupUsers;
+
+public record SignupUserCommand(string Email, string PhoneNumber, string Password) : IRequest;
+
+public class SignupUserCommandHandler : IRequestHandler<SignupUserCommand>
+{
+    private readonly IApplicationDbContext _dbContext;
+    private readonly IIdentityService _identityService;
+
+    public SignupUserCommandHandler(IApplicationDbContext dbContext, IIdentityService identityService)
+    {
+        _dbContext = dbContext;
+        _identityService = identityService;
+    }
+
+    public async Task<Unit> Handle(SignupUserCommand request, CancellationToken cancellationToken)
+    {
+        var (result, userId) = await _identityService.CreateUserAsync(
+            request.Email, 
+            request.PhoneNumber, 
+            request.Password);
+
+        if (!result.Succeeded) 
+            throw new Exception("Problem creating user");
+
+        await _identityService.AddUserToRole(userId, AppRolesConstants.UserRole);
+        
+        return Unit.Value;
+    }
+}
---
+++ src/WebApi/Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using Application.ApplicationUsers.Commands.SignupUsers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+public class AuthController : ApiControllerBase
+{
+    [HttpPost("sign-up")]
+    public async Task<ActionResult> SignUpUser(SignupUserCommand command)
+    {
+        await Mediator.Send(command);
+
+        return NoContent();
+    }
+}
