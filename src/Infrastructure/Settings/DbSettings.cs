644c12085dee2b609c67adcaa2a2e8eee904ba2a
Gordey Stakhovets <gordeystahovets@gmail.com>
2022-10-11 22:36:10 +0300

    feat(settings): add database settings and netescapades library for validating settings

---
+++ src/Infrastructure/Settings/DbSettings.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using NetEscapades.Configuration.Validation;
+
+namespace Microsoft.Extensions.DependencyInjection.Startup.Settings;
+
+public class DbSettings : IValidatable
+{
+    [Required]
+    public string Host { get; set; }
+
+    [Required]
+    public int Port { get; set; }
+
+    [Required]
+    public string DatabaseName { get; set; }
+
+    [Required]
+    public string UserId { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+
+    public string ConnectionString => $"Server={Host};Port={Port};Database={DatabaseName};User Id={UserId};Password={Password};";
+
+    public void Validate()
+    {
+        Validator.ValidateObject(this, new ValidationContext(this), true);
+    }
+}
---
+++ src/WebApi/Settings/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using NetEscapades.Configuration.Validation;
+
+namespace Microsoft.Extensions.DependencyInjection.Startup.Settings;
+
+public class AppSettings : IValidatable
+{
+    public DbSettings DbSettings { get; set; }
+    
+    public void Validate()
+    {
+        DbSettings.Validate();
+    }
+}
