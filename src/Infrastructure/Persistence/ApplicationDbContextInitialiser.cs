e321731fe99864947a436a48db6e2561a431d528
Gordey Stakhovets <gordeystahovets@gmail.com>
2022-10-12 00:44:35 +0300

    feat(database): add initial initialiser for users and roles

---
+++ src/Infrastructure/Persistence/ApplicationDbContextInitialiser.cs
@@ -0,0 +1,108 @@
+﻿using Application.Constants;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Persistence;
+
+public class ApplicationDbContextInitialiser
+{
+    private readonly ILogger<ApplicationDbContextInitialiser> _logger;
+    private readonly ApplicationDbContext _context;
+    private readonly UserManager<AppUser> _userManager;
+    private readonly RoleManager<AppRole> _roleManager;
+
+    public ApplicationDbContextInitialiser(
+        ILogger<ApplicationDbContextInitialiser> logger, 
+        ApplicationDbContext context, 
+        UserManager<AppUser> userManager, 
+        RoleManager<AppRole> roleManager)
+    {
+        _logger = logger;
+        _context = context;
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
+    
+     public async Task InitialiseAsync()
+    {
+        try
+        {
+            if (_context.Database.IsNpgsql())
+            {
+                await _context.Database.MigrateAsync();
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while initialising the database.");
+            throw;
+        }
+    }
+
+    public async Task SeedAsync()
+    {
+        try
+        {
+            await TrySeedAsync();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while seeding the database.");
+            throw;
+        }
+    }
+
+    public async Task TrySeedAsync()
+    {
+        // Default roles
+        var adminRole = new AppRole
+        {
+            Name = AppRolesConstants.AdminRole
+        };
+        
+        var userRole = new AppRole
+        {
+            Name = AppRolesConstants.UserRole
+        };
+
+        if (_roleManager.Roles.All(r => r.Name != adminRole.Name))
+        {
+            await _roleManager.CreateAsync(adminRole);
+        }
+        
+        if (_roleManager.Roles.All(r => r.Name != userRole.Name))
+        {
+            await _roleManager.CreateAsync(userRole);
+        }
+
+        // Default users
+        var administrator = new AppUser
+        {
+            Id = Guid.NewGuid(),
+            UserName = "administrator@localhost", 
+            Email = "administrator@localhost",
+            SecurityStamp = Guid.NewGuid().ToString()
+        };
+        var user = new AppUser
+        {
+            Id = Guid.NewGuid(),
+            UserName = "user@localhost", 
+            Email = "user@localhost",
+            SecurityStamp = Guid.NewGuid().ToString()
+        };
+
+        if (_userManager.Users.All(u => u.UserName != administrator.UserName))
+        {
+            await _userManager.CreateAsync(administrator, "admin1234");
+            await _userManager.AddToRolesAsync(administrator, new[] { adminRole.Name });
+        }
+        
+        if (_userManager.Users.All(u => u.UserName != user.UserName))
+        {
+            await _userManager.CreateAsync(user, "user1234");
+            await _userManager.AddToRolesAsync(user, new[] { userRole.Name });
+        }
+    }
+}
--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,5 +1,9 @@
+using Application;
 using Infrastructure;
-using Microsoft.Extensions.DependencyInjection.Startup.Settings;
+using Infrastructure.Persistence;
+using Infrastructure.Settings;
+using WebApi;
+using WebApi.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,14 +16,28 @@ var appSettings = new AppSettings
     DbSettings = dbSettings
 };
 
-builder.Services.UseConfigurationValidation();
-builder.Services.ConfigureValidatableSetting<AppSettings>(builder.Configuration);
-
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(appSettings.DbSettings);
-builder.Services.AddWebApiServices();
+builder.Services.AddWebApiServices(builder.Configuration);
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+
+    // Initialise and seed database
+    using var scope = app.Services.CreateScope();
+    
+    var initialiser = scope.ServiceProvider.GetRequiredService<ApplicationDbContextInitialiser>();
+    await initialiser.InitialiseAsync();
+    await initialiser.SeedAsync();
+}
+else
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
 app.UseHealthChecks("/health");
 app.UseHttpsRedirection();
