1a7afba2936c6826282c980577529cc247b6980b
Gordey Stakhovets <gordeystahovets@gmail.com>
2022-10-12 03:52:35 +0300

    refactor: change identity result wrapper name

---
+++ src/Application/Common/Models/IdentityResult.cs
@@ -0,0 +1,24 @@
+﻿namespace Application.Common.Models;
+
+public class IdentityResult
+{
+    private IdentityResult(bool succeeded, IEnumerable<string> errors)
+    {
+        Succeeded = succeeded;
+        Errors = errors.ToArray();
+    }
+
+    public bool Succeeded { get; set; }
+
+    public string[] Errors { get; set; }
+
+    public static IdentityResult Success()
+    {
+        return new IdentityResult(true, Array.Empty<string>());
+    }
+
+    public static IdentityResult Failure(IEnumerable<string> errors)
+    {
+        return new IdentityResult(false, errors);
+    }
+}
--- src/Infrastructure/Identity/IdentityResultExtensions.cs
+++ src/Infrastructure/Identity/IdentityResultExtensions.cs
@@ -1,14 +1,15 @@
 ﻿using Application.Common.Models;
 using Microsoft.AspNetCore.Identity;
+using IdentityResult = Application.Common.Models.IdentityResult;
 
 namespace Infrastructure.Identity;
 
 public static class IdentityResultExtensions
 {
-    public static Result ToApplicationResult(this IdentityResult result)
+    public static IdentityResult ToApplicationResult(this Microsoft.AspNetCore.Identity.IdentityResult result)
     {
         return result.Succeeded
-            ? Result.Success()
-            : Result.Failure(result.Errors.Select(e => e.Description));
+            ? IdentityResult.Success()
+            : IdentityResult.Failure(result.Errors.Select(e => e.Description));
     }
 }
