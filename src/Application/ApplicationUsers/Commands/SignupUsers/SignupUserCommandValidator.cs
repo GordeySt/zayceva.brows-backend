9a5033721701a8f351ff6e92e580eb5a3ad54126
Gordey Stakhovets <gordeystahovets@gmail.com>
2022-10-12 05:27:16 +0300

    feat(validation): add validation for sign up user command

---
+++ src/Application/ApplicationUsers/Commands/SignupUsers/SignupUserCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using Application.Common.Validators;
+using FluentValidation;
+
+namespace Application.ApplicationUsers.Commands.SignupUsers;
+
+public class SignupUserCommandValidator : AbstractValidator<SignupUserCommand>
+{
+    public SignupUserCommandValidator()
+    {
+        RuleFor(v => v.Email).Email();
+        RuleFor(v => v.Password).Password();
+        RuleFor(v => v.FirstName).FirstName();
+        RuleFor(v => v.LastName).LastName();
+        RuleFor(v => v.PhoneNumber).PhoneNumber();
+    }
+}
---
+++ src/Application/Common/Validators/FluentValidatorExtensions.cs
@@ -0,0 +1,44 @@
+﻿using FluentValidation;
+
+namespace Application.Common.Validators;
+
+public static class FluentValidatorExtensions
+{
+    private const string BelarusPhoneNumberRegex = @"^\+375 \((17|29|33|44)\) [0-9]{3}-[0-9]{2}-[0-9]{2}$";
+    
+    public static void Password<T>(this IRuleBuilder<T, string> ruleBuilder)
+    {
+        ruleBuilder
+            .NotEmpty().WithMessage("Password must not be empty")
+            .MinimumLength(7).WithMessage("Password length must be greater than 6");
+    }
+
+    public static void Email<T>(this IRuleBuilder<T, string> ruleBuilder)
+    {
+        ruleBuilder
+            .NotEmpty().WithMessage("Email must not be empty")
+            .EmailAddress().WithMessage("Invalid Email address");
+    }
+    
+    public static void FirstName<T>(this IRuleBuilder<T, string> ruleBuilder)
+    {
+        ruleBuilder
+            .NotEmpty().WithMessage("First name must not be empty")
+            .MaximumLength(30).WithMessage("First name length must be less than 30");
+    }
+    
+    public static void LastName<T>(this IRuleBuilder<T, string> ruleBuilder)
+    {
+        ruleBuilder
+            .NotEmpty().WithMessage("Last name must not be empty")
+            .MaximumLength(50).WithMessage("Last name length must be less than 50");
+    }
+    
+    public static void PhoneNumber<T>(this IRuleBuilder<T, string> ruleBuilder)
+    {
+        ruleBuilder
+            .NotEmpty().WithMessage("Phone number must not be empty")
+            .Matches(BelarusPhoneNumberRegex)
+            .WithMessage("Phone number format is invalid");
+    }
+}
