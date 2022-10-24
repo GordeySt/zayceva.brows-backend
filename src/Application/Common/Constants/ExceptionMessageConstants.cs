cf9fab26ac970d8ffb00ce4c037a208f7beb0940
Gordey Stakhovets <gordeystahovets@gmail.com>
2022-10-24 18:54:29 +0300

    feat: add NotFoundUser and ProblemVerifyingEmail constant messages

--- src/Application/Common/Constants/ExceptionMessageConstants.cs
+++ src/Application/Common/Constants/ExceptionMessageConstants.cs
@@ -1,8 +1,14 @@
 ﻿namespace Application.Common.Constants;
 
+public static class NotFoundExceptionMessageConstants
+{
+    public const string NotFoundUserMessage = "User with the following id not found";
+}
+
 public static class BadRequestExceptionMessageConstants
 {
     public const string ExistedUserMessage = "User with the following email already exists";
+    public const string ProblemVerifyingEmailMessage = "Problem verifying message";
 }
 
 public static class InternalServerErrorExceptionMessageConstants
