d62fbf4625cc5db1ec45f98dd91d43b26f797f60
Hardzei <hardzei.stakhavets@rosecut.com>
2022-11-07 23:46:21 +0300

    feat: add sign in validation

---
+++ src/Application/ApplicationUsers/Queries/SignInUsers/SignInUserQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using Application.Common.Validators;
+using FluentValidation;
+
+namespace Application.ApplicationUsers.Queries.SignInUsers;
+
+public class SignInUserQueryValidator : AbstractValidator<SignInUserQuery>
+{
+    public SignInUserQueryValidator()
+    {
+        RuleFor(v => v.Email).Email();
+        RuleFor(v => v.Password).Password();
+    }
+}
